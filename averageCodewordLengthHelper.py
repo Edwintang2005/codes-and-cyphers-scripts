e0cb953f3f8d9ffd60860206fca5675985d1620a
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2024-10-24 18:21:50 +1100

    Added a helper for calcuating lengths of markov sources

---
+++ averageCodewordLengthHelper.py
@@ -0,0 +1,23 @@
+# shortcut tool FOR MARKOV SOURCES - Transition matrixes and equilibrium vectors
+from fractions import Fraction
+
+sourceLengths = []
+equilibriumVector = []
+# Collecting lengths for each source
+index = 0
+while True:
+    inputStr = input(f"l{index + 1}: ")
+    if inputStr == '':
+        break
+    sourceLengths.append(Fraction(inputStr))
+    index += 1
+# Collecting equilibrium vector
+index = 0
+for i in sourceLengths:
+    equilibriumVector.append(Fraction(input(f"Equilibirum vector position {index + 1}: ")))
+    index += 1
+sum = 0
+# Finding dot product of the two vectors
+for i in range(0, len(sourceLengths)):
+    sum += sourceLengths[i] * equilibriumVector[i]
+print(sum)
